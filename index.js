e3ea328a35b2871e2a9138005675e8e8b12f449c
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-12-14 11:14:23 +0100

    Basic implementation and spec test

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ var cccf  = require('cccf')
 var clone = require('clone')
 
 var scaleUp = function(config) {
-	return config
+	return config.reduce(function(containers, container) {
+		containers.push(container)
+		if (!container.scale) return containers
+		if (typeof container.scale != 'number') return containers
+		var i = 1
+		while(i < container.scale) {
+			var scale = clone(container)
+			delete scale.scale
+			scale.id = scale.id+'-scale-'+i
+			containers.push(scale)
+			i++
+		}
+		return containers
+	}, [])
+}
+
+var scaleDown = function(config) {
+	return config.reduce(function(containers, container) {
+		if (container.id.indexOf('-scale-') > 0) return containers
+		containers.push(container)
+		return containers
+	}, [])
 }
 
 module.exports = {
@@ -12,6 +33,8 @@ module.exports = {
 		return scaleUp(config)
 	},
 	down : function(config) {
-
+		if (!(config instanceof Array)) config = [config]
+		if (cccf.validate(config)) return cccf.validate(config)
+		return scaleDown(config)
 	}
 }
--- test/spec.js
+++ test/spec.js
@@ -10,9 +10,17 @@ describe('cccf-scale', function() {
 
 	it('can scale up', function() {
 		var upscaled = cs.up(config)
-		console.log(upscaled)
 		assert(upscaled != null)
 		assert(upscaled.length == 10)
+	})
+
+	it('can scale down', function() {
+		var up = cs.up(config)
+		assert(up != null)
+		assert(up.length == 10)
+		var down = cs.down(up)
+		assert(down != null)
+		assert(down.length == 1)
 	})
 
 })
